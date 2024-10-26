e9a26889030ad5886befd492e381c36ce0a236ae
likongpeng <likongpeng@bytedance.com>
2024-10-26 20:59:06 +0800

    bugfix

--- src/main/java/ddd/base/domain/StringTypeHandler.java
+++ src/main/java/ddd/base/domain/StringTypeHandler.java
@@ -1,15 +1,17 @@
 package ddd.base.domain;
 
 import com.alibaba.fastjson.JSON;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.JdbcType;
-
+import com.alibaba.fastjson.JSONException;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
 
 @Slf4j
 public class StringTypeHandler extends BaseTypeHandler<Object> {
@@ -23,6 +25,19 @@ public class StringTypeHandler extends BaseTypeHandler<Object> {
 	public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
 		try{
 			return JSON.parseObject(rs.getString(columnName), List.class);
+		}catch(JSONException e){
+			//兼容历史数据
+			if(StringUtils.isNotBlank(e.getMessage()) && e.getMessage().contains("exepct '[', but ident,")){
+				String value = rs.getString(columnName);
+				if(StringUtils.isBlank(value)){
+					return null;
+				}
+				List<String> list = new ArrayList<>();
+				list.add(value);
+				return list;
+			}
+			log.info("getNullableResultError context is  {},column is {}",rs.getString(columnName),columnName);
+			throw e;
 		}catch(Exception e){
 		    log.info("getNullableResultError context is  {},column is {}",rs.getString(columnName),columnName);
 		    throw e;
