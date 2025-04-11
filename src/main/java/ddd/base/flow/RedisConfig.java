ef9bf36ba1f8e02c1b66676ca43d234f764a6234
文贺 <luanqiu@yeah.net>
2025-04-11 17:17:55 +0800

    修改

--- src/main/java/ddd/base/flow/RedisConfig.java
+++ src/main/java/ddd/base/flow/RedisConfig.java
@@ -8,11 +8,11 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
-
+    
     @Bean
     public JedisConnectionFactory redisConnectionFactory() {
         JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setHostName("172.25.9.10");
+        factory.setHostName("10.10.83.6");
         factory.setPort(6379);
         factory.setPassword("123456789");
         return factory;
